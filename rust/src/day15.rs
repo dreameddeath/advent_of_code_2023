5ab6e7b5308164f8225f20260dee3310a7014fd7
christophe jeunesse <christophe.jeunesse@orange.com>
2023-12-15 07:51:23 +0100

    [Day 15] mini refacto

--- rust/src/day15.rs
+++ rust/src/day15.rs
@@ -9,15 +9,10 @@ fn calc_hash(str: &str) -> u8 {
     str.chars().fold(0 as u16, |h, c| ((h + c as u16) * 17) % 256) as u8
 }
 
-pub fn puzzle(context: &Context, lines: &Vec<String>) {
-    let items = lines[0].split(",");
-    if context.is_part(Part::Part1) {
-        let result = items.map(|part| calc_hash(part) as u32).sum();
-        check_result!(context, result, [1320, 511215]);
-    } else {
-        let mut boxes: BTreeMap<u8, Vec<(&str, u8)>> = BTreeMap::new();
-        items.for_each(|part| {
-            let op_pos = part.find(|c| c == '=' || c == '-').unwrap();
+type BoxesMap<'a> = BTreeMap<u8, Vec<(&'a str, u8)>>;
+
+fn update_box<'a>(part:&'a str,boxes :&mut BoxesMap<'a>){
+    let op_pos = part.find(|c| c == '=' || c == '-').unwrap();
             let op = part.chars().nth(op_pos).unwrap();
             let lens = &part[0..op_pos];
             let box_id = calc_hash(lens);
@@ -39,6 +34,17 @@ pub fn puzzle(context: &Context, lines: &Vec<String>) {
                 }
                 _ => box_found.retain(|curr| curr.0 != lens),
             }
+}
+
+pub fn puzzle(context: &Context, lines: &Vec<String>) {
+    let items = lines[0].split(",");
+    if context.is_part(Part::Part1) {
+        let result = items.map(|part| calc_hash(part) as u32).sum();
+        check_result!(context, result, [1320, 511215]);
+    } else {
+        let mut boxes: BoxesMap = BTreeMap::new();
+        items.for_each(|part| {
+            update_box(part, &mut boxes)
         });
 
         let result: u32 = boxes
